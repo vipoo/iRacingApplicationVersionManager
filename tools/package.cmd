91aeb41af1c1813a183028fabda7cb7648cb7218
Dean Netherton <dean.netherton@gmail.com>
2016-01-30 12:34:41 +1100

    Fixed click-once url

--- tools/package.cmd
+++ tools/package.cmd
@@ -6,7 +6,7 @@ msbuild iRacingApplicationVersionManager.csproj -p:SolutionDir=%cd%\            
                                         -p:Configuration=Release                                                                ^
                                         -t:rebuild                                                                              ^
                                         -t:publish                                                                              ^
-                                        -p:"InstallUrl=https://iracing-application-version-manager.s3-website-ap-southeast-2.amazonaws.com/release/"  ^
+                                        -p:"InstallUrl=https://s3-ap-southeast-2.amazonaws.com/iracing-application-version-manager/release/"  ^
                                         -p:ApplicationVersion=%APPVEYOR_BUILD_VERSION%                                          ^
                                         -v:minimal                                          
  
