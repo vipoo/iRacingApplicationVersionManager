92f10a9dacc9a5fafe5840691d7444465121f1a3
Dean Netherton <dean.netherton@gmail.com>
2016-01-30 14:39:12 +1100

    Deployment: include missing file TaskScheduler dll

--- tools/package.cmd
+++ tools/package.cmd
@@ -18,6 +18,7 @@ for /D %%F in ("Application Files\*") do (
     appveyor PushArtifact "%%F\iRacingApplicationVersionManger.pdb.deploy"        -FileName "release\%%F\iRacingApplicationVersionManger.pdb.deploy"        -DeploymentName deploy
     appveyor PushArtifact "%%F\iRacingApplicationVersionManger.exe.manifest"      -FileName "release\%%F\iRacingApplicationVersionManger.exe.manifest"      -DeploymentName deploy 
     appveyor PushArtifact "%%F\Octokit.dll.deploy"                                -FileName "release\%%F\Octokit.dll.deploy"                                -DeploymentName deploy 
+    appveyor PushArtifact "%%F\Microsoft.Win32.TaskScheduler.dll.deploy"          -FileName "release\%%F\Microsoft.Win32.TaskScheduler.dll.deploy"          -DeploymentName deploy 
     appveyor PushArtifact "%%F\Octokit.pdb.deploy"                                -FileName "release\%%F\Octokit.pdb.deploy"                                -DeploymentName deploy 
 )
 
