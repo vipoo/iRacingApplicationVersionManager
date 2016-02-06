66f855d10e1d2ca929de2814e0d2026911e92904
Dean Netherton <dean.netherton@gmail.com>
2016-02-06 22:52:31 +1100

    Fixes for namespaces and saving of githubreleases cache

---
+++ Properties/GitHubAccessSettings.cs
@@ -0,0 +1,22 @@
+﻿using GitHubReleases;
+using iRacingApplicationVersionManger.Properties;
+
+internal static class GitHubAccessSettings
+{
+    public static GitHubCachedReleases[] GitHubCachedReleases
+    {
+        get
+        {
+            return Settings.Default.GitHubCachedReleases;
+        }
+        set
+        {
+            Settings.Default.GitHubCachedReleases = value;
+        }
+    }
+
+    public static void Save()
+    {
+        Settings.Default.Save();
+    }
+}
